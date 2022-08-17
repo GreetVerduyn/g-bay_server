040b23cbd77a781cab45800be17d336ee0a7e39e
Greet Verduyn <greetverduyn@proximus.be>
2022-08-17 16:00:10 +0200

    adding nodes to gitignore

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello guys from server!" });
 });
 
 app.listen(PORT, () => {
