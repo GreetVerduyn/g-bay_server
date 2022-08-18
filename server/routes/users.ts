4856103897e3448d4060b97f917836900078a53b
Greet Verduyn <greetverduyn@proximus.be>
2022-08-18 10:18:13 +0200

    create a getter for all users

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -3,9 +3,6 @@ import controller from '../controllers/users';
 const router = express.Router();
 
 router.get('/users', controller.getUsers);
-router.get('/users/:id', controller.getUser);
-router.put('/users/:id', controller.updateUser);
-router.delete('/users/:id', controller.deleteUser);
-router.post('/users', controller.addUser);
+
 
 export = router;
